ca0469f1987edfa265d04b35d67c0483a7f1fdb5
Your Name <you@example.com>
2018-11-06 00:05:04 +0100

    Add RelativeLayout, background and ImageView

--- app/src/main/java/com/example/luc_olivier/howifeel/Controller/MainActivity.java
+++ app/src/main/java/com/example/luc_olivier/howifeel/Controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.luc_olivier.howifeel;
+package com.example.luc_olivier.howifeel.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.luc_olivier.howifeel.R;
 
 public class MainActivity extends AppCompatActivity {
 
