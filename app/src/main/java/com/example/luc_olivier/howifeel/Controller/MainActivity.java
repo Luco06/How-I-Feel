106c49714f7eccc23551a7f2f19b485617cc7c49
Your Name <you@example.com>
2018-11-26 17:22:29 +0100

    Add list color and img

--- app/src/main/java/com/example/luc_olivier/howifeel/Controller/MainActivity.java
+++ app/src/main/java/com/example/luc_olivier/howifeel/Controller/MainActivity.java
@@ -8,23 +8,32 @@ import android.widget.ImageButton;
 import com.example.luc_olivier.howifeel.R;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int [][] LIST_COLOR_IMG = {
+            {R.color.faded_red,
+                    R.color.warm_grey,
+                    R.color.cornflower_blue_65,
+                    R.color.light_sage,
+                    R.color.banana_yellow,},
 
-    private ImageButton mBtnhistory;
-    private ImageButton mBtncomment;
+            {R.drawable.smiley_sad,
+                    R.drawable.smiley_disappointed,
+                    R.drawable.smiley_normal,
+                    R.drawable.smiley_happy,
+                    R.drawable.smiley_super_happy}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBtncomment = (ImageButton) findViewById(R.id.comment_main);
-        mBtnhistory = (ImageButton) findViewById(R.id.history_main);
+        ImageButton btnComment = (ImageButton) findViewById(R.id.comment_main);
+        ImageButton btnHistory = (ImageButton) findViewById(R.id.history_main);
 
         /*
          *@mBtnComment
          * Button to the left side, which open an Alert dialog on click
          */
-        mBtncomment.setOnClickListener(new View.OnClickListener() {
+        btnComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addComment();
